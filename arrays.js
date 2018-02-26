3f663d537e1bb2eb17b9989ab23721f5c1681cef
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-02-26 03:55:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(a, b) {
   var c = a.unshift(b)
-  return c
+  return 
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b) {
